8d43c271dd774b460a7ac3488c989b933d3263ed
ktgl <40495186+ktgl@users.noreply.github.com>
2018-06-22 16:03:45 +0100

    Updating GenerateMessage function DM

--- src/AutoDM.js
+++ src/AutoDM.js
@@ -4,7 +4,7 @@ const timeout = 1000 * 60 * 0.2; // timeout to send the message in 12 seconds
 
 const AutoDM = () => {
   const stream = T.stream("user");
-  console.log("Start Sending Auto Direct Message 🚀🚀🚀");
+  console.log("Auto Direct Message initialised...waiting for follows 🚀🚀🚀");
   stream.on("follow", SendMessage);
 };
 
@@ -17,14 +17,14 @@ const SendMessage = user => {
   };
   // the follow stream track if I follow author person too.
   if (screen_name != my_user_name) {
-    console.log(" 🎉🎉🎉🎉 New Follower  🎉🎉🎉🎉🎉 ");
+    console.log(" 🎉🎉🎉🎉 New Follower - ${screen_name} 🎉🎉🎉🎉🎉 ");
     setTimeout(() => {
       T.post("direct_messages/new", obj)
         .catch(err => {
           console.error("error", err.stack);
         })
         .then(result => {
-          console.log(`Message sent successfully To  ${screen_name}  💪💪`);
+          console.log(`Message sent successfully to ${screen_name}  💪💪`);
         });
     }, timeout);
   }
@@ -41,11 +41,12 @@ const GenerateMessage = name => {
   ];
   const d = new Date();
   const dayName = days[d.getDay()];
-  return `Hi ${name} Thanks for following! Need any #ITsupport or #Contrex? 
+  return `Hi ${name} Thanks for following! Need #ITsupport or #Contrex VoIP telephone systems? 
 
 KTGL is a #Hull based company supporting local businesses for 20 years.
-Contact us on 01482 291291. We'll be happy to assist! 
+Contact us on 01482 291291, email us at support@ktgl.co.uk, or check us out at http://www.ktgl.co.uk/. 
 
+We'll be happy to assist! 
 
 Thanks
 KTGL      
